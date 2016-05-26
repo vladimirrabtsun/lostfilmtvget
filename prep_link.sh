9e8969464849bb772eaa6ace0540a8d820db9eb7
Vladimir Rabtsun <vladimir.rabtsun@gmail.com>
2016-05-26 16:35:38 +0600

    add code

--- prep_link.sh
+++ prep_link.sh
@@ -4,9 +4,44 @@ cat tmp.2 | grep -ie '\(Fear.the.Walking.Dead\|Mr.Robot\|The.Walking.Dead\|The.X
 
 # Определяем номер созона и номер серии.
 cat selected.serials.tmp | grep -m 1 -oE '\S[[:digit:]]{1,2}\E[[:digit:]]{1,2}' >> current.ses_ep.selected.serials.tmp
-ser_s=cat current.ses_ep.selected.serials.tmp | cut -c 2-3
-ser_e=cat current.ses_ep.selected.serials.tmp | cut -c 5-6
 
+ser_s_1_2=cat current.ses_ep.selected.serials.tmp | cut -c 2-3
+ser_s_1=cat current.ses_ep.selected.serials.tmp | cut -c 2-2
+ser_s_2=cat current.ses_ep.selected.serials.tmp | cut -c 3-3
+ser_e_1_2=cat current.ses_ep.selected.serials.tmp | cut -c 5-6
+ser_e_1=cat current.ses_ep.selected.serials.tmp | cut -c 5-5
+ser_e_2=cat current.ses_ep.selected.serials.tmp | cut -c 6-6
+
+if [ $ser_s_1 = 0 ]; then
+	ser_s_post=$ser_s_2
+else
+	ser_s_post=$ser_s_1_2
+fi
+
+ser_s_selected=0
+if [ !$ser_s_post ]; then
+	echo "Ошибка (Код:3). Переменная ser_s_post не назначена."
+else
+	$ser_s_selected=1
+	echo "[OK] Определение номера серии сериала: ser_s_post = $ser_s_post"
+fi
+
+if [ $ser_e_1 = 0 ]; then
+	ser_e_post=$ser_e_2
+else
+	ser_e_post=$ser_e_1_2
+fi
+
+ser_e_selected=0
+if [ !$ser_e_post ]; then
+        echo "Ошибка (Код:4). Переменная ser_e_post не назначена."
+else
+        $ser_e_selected=1
+        echo "[OK] Определение номера сезона сериала: ser_e_post = $ser_e_post"
+fi
+
+
+# Определяем наименование сериала и его порядковый номер на lostfilm.tv
 ser_name=cat tmp.2 | grep -oie '\(Fear.the.Walking.Dead\|Mr.Robot\|The.Walking.Dead\|The.X-Files\|Person.of.Interest\)'
 
 if [ $ser_name = "Fear.the.Walking.Dead" ]; then
