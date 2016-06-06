c8c8f49dc148fa31f8153d4fbf8729b32710e1fa
Vladimir Rabtsun <vladimir.rabtsun@gmail.com>
2016-06-06 18:02:12 +0600

    add code

--- get_serials.sh
+++ get_serials.sh
@@ -1,29 +1,28 @@
-
-    #!/bin/sh
-    TMP1=tmp.1
-    TMP2=tmp.2
-    TMP3=tmp.3
-    lfrss="http://www.lostfilm.tv/rssdd.xml"
-    lfcookie="Cookie: _ga=GA1.2.1172720830.1460950075; _gat=1; _ym_isad=2; _ym_uid=1460950075248392930; _ym_visorc_5681512=w; lnk_uid=95d4f4b893a5be0fd044052e4ab61dad; pass=c1590eb43b93eef263dbbb18bff69adc; phpbb2mysql_data=a%3A2%3A%7Bs%3A11%3A%22autologinid%22%3Bs%3A32%3A%22b129187c8bc7e421477dcd2c578bdda0%22%3Bs%3A6%3A%22userid%22%3Bs%3A7%3A%224721761%22%3B%7D; uid=4762870"
-    ua="Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
-    # Каталог, который наблюдает transmission
-    WDIR=~/Downloads/torrents_lostfilm_tv
-    # Качаем RSS
-    /usr/bin/wget -O $TMP1 $lfrss
-    # Качать только avi
-    cat $TMP1 | grep -ioe 'http.*torrent'| grep -ie '1080p' > $TMP2
-    # Список сериалов
-    cat $TMP2 | grep -ie '\(Fear\|The.Originals\|Mr.Robot\|The.Walking.Dead\|The.X-Files\|Person\)' > $TMP3
-    for l in `cat $TMP3`
-    do
-    tor=`echo $l | cut -d';' -f2`
-    if [ ! -f $WDIR/$tor ]
-    then
-    echo !!! download $tor
-    /usr/bin/wget -nc -O $tor --referer="$lfrss" --user-agent="$ua" --no-cookies --header="$lfcookie" $l
-    echo ------------ >> download.torrent.list
-    echo `date`: download $tor >> download.torrent.list
-    mv $tor $WDIR
-    fi
-    done
+#!/bin/sh
+TMP1=tmp.1
+TMP2=tmp.2
+TMP3=tmp.3
+lfrss="http://www.lostfilm.tv/rssdd.xml"
+lfcookie="Cookie: _ga=GA1.2.1172720830.1460950075; _gat=1; _ym_isad=2; _ym_uid=1460950075248392930; _ym_visorc_5681512=w; lnk_uid=95d4f4b893a5be0fd044052e4ab61dad; pass=c1590eb43b93eef263dbbb18bff69adc; phpbb2mysql_data=a%3A2%3A%7Bs%3A11%3A%22autologinid%22%3Bs%3A32%3A%22b129187c8bc7e421477dcd2c578bdda0%22%3Bs%3A6%3A%22userid%22%3Bs%3A7%3A%224721761%22%3B%7D; uid=4762870"
+ua="Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
+# Каталог, который наблюдает transmission
+WDIR=~/Downloads/torrents_lostfilm_tv
+# Качаем RSS
+/usr/bin/wget -O $TMP1 $lfrss
+# Качать только avi
+cat $TMP1 | grep -ioe 'http.*torrent'| grep -ie '1080p' > $TMP2
+# Список сериалов
+cat $TMP2 | grep -ie '\(Fear\|The.Originals\|Mr.Robot\|The.Walking.Dead\|The.X-Files\|Person\)' > $TMP3
+for l in `cat $TMP3`
+do
+tor=`echo $l | cut -d';' -f2`
+if [ ! -f $WDIR/$tor ]
+then
+echo !!! download $tor
+/usr/bin/wget -nc -O $tor --referer="$lfrss" --user-agent="$ua" --no-cookies --header="$lfcookie" $l
+echo ------------ >> download.torrent.list
+echo `date`: download $tor >> download.torrent.list
+mv $tor $WDIR
+fi
+done
 
--- new_get_serials.sh
+++ new_get_serials.sh
@@ -1,7 +1,20 @@
-#update 1
-# Забираем в файл selected.serials.tmp только искомые сериалы по названию.
-cat tmp.2 | grep -ie '\(Fear.the.Walking.Dead\|Mr.Robot\|The.Walking.Dead\|The.X-Files\|Person.of.Interest\)' >> selected.serials.tmp
+#!/bin/sh
+TMP1=tmp.1
+TMP2=tmp.2
+TMP3=tmp.3
+lfrss="http://www.lostfilm.tv/rssdd.xml"
+lfcookie="Cookie: _ga=GA1.2.1172720830.1460950075; _gat=1; _ym_isad=2; _ym_uid=1460950075248392930; _ym_visorc_5681512=w; lnk_uid=95d4f4b893a5be0fd044052e4ab61dad; pass=c1590eb43b93eef263dbbb18bff69adc; phpbb2mysql_data=a%3A2%3A%7Bs%3A11%3A%22autologinid%22%3Bs%3A32%3A%22b129187c8bc7e421477dcd2c578bdda0%22%3Bs%3A6%3A%22userid%22%3Bs%3A7%3A%224721761%22%3B%7D; uid=4762870"
+ua="Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
+# Каталог, который наблюдает transmission
+WDIR=~/Downloads/torrents_lostfilm_tv
+# Качаем RSS
+/usr/bin/wget -O $TMP1 $lfrss
+# Качать только avi
+cat $TMP1 | grep -ioe 'http.*torrent'| grep -ie '1080p' > $TMP2
 
+##########
+
+#update 1
 declare -a sc
 sc=(`cat tmp.2 | grep -ie '\(Fear.the.Walking.Dead\|Mr.Robot\|The.Walking.Dead\|The.X-Files\|Person.of.Interest\)' | tr '\n' ' '`)
 
@@ -119,15 +132,13 @@ fi
 
 # Формируем ссылку на страницу для загрузки торрент-файлов, передавая атрибуты: c, s и e.
 if [ "$ready_to_gen_link" = "$true_argument" ]; then
-	link_to_download_page="https://lostfilm.tv/nrdr2.php?c=$ser_c&s=$ser_s_post&e=$ser_e_post"
+	link_to_dl_page_stage="https://lostfilm.tv/nrdr2.php?c=$ser_c&s=$ser_s_post&e=$ser_e_post"
 	echo "[OK] Определение страницы с ссылками для загрузки: $link_to_download_page"
 else
 	echo "Ошибка (Код:5). Неопредены необходимые переменные."
 fi
 
-echo $link_to_download_page >> list_to_download.tmp
 
-done
 
 # Удаляем временные файлы
 
@@ -139,3 +150,51 @@ done
 
 #rm current.ses_ep.selected.serials.tmp
 #echo "[OK] Удаление временного файла current.ses_ep.selected.serials.tmp"
+
+##########
+
+##########
+
+lfcookie="Cookie: _ga=GA1.2.1172720830.1460950075; _gat=1; _ym_isad=2; _ym_uid=1460950075248392930; _ym_visorc_5681512=w; lnk_uid=95d4f4b893a5be0fd044052e4ab61dad; pass=c1590eb43b93eef263dbbb18bff69adc; phpbb2mysql_data=a%3A2%3A%7Bs%3A11%3A%22autologinid%22%3Bs%3A32%3A%22b129187c8bc7e421477dcd2c578bdda0%22%3Bs%3A6%3A%22userid%22%3Bs%3A7%3A%224721761%22%3B%7D; uid=4762870"
+ua="Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
+
+/usr/bin/wget -O dl_page_stage.tmp $link_to_dl_page_stage --user-agent="$ua" --no-cookies --header="$lfcookie" $l
+
+link_to_dl_page_final=$(cat dl_page_stage.tmp | grep -ie 'location.replace("http://retre.org' | grep -ioe '(".*")' | cut -c 3- | rev | cut -c 3- | rev)
+
+#echo "[DEBUG] link_to_dl_page_final=$link_to_dl_page_final" # Отладка
+
+if [ -e "$link_to_dl_page_final" ]; then
+	echo "[ERROR] Ошибка (Код:9). Переменная link_to_dl_page_final не назначена."
+else
+	echo "[OK] Определение ссылки: link_to_dl_page_final=$link_to_dl_page_final"
+fi
+
+lfrss1=$link_to_dl_page_final
+
+/usr/bin/wget -O dl_page_final.tmp $lfrss1 --user-agent="$ua" --no-cookies --header="$lfcookie" $l
+
+link_to_dl_torrent=$(cat dl_page_final.tmp | grep -ie '1080p WEB-DLRip\.' -A 1 | grep -ioe '<a href=".*" ' | cut -c 10- | rev | cut -c 3- | rev)
+
+#echo "[DEBUG] link_to_dl_torrent=$link_to_dl_torrent" # Отладка
+
+
+echo $link_to_download_page >> list_to_download.tmp
+
+done
+
+##########
+
+for l in `cat $TMP3`
+do
+tor=`echo $l | cut -d';' -f2`
+if [ ! -f $WDIR/$tor ]
+then
+echo !!! download $tor
+/usr/bin/wget -nc -O $tor --referer="$lfrss" --user-agent="$ua" --no-cookies --header="$lfcookie" $l
+echo ------------ >> download.torrent.list
+echo `date`: download $tor >> download.torrent.list
+mv $tor $WDIR
+fi
+done
+
